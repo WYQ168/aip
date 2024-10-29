15f802566534f916bcb422b15c19723510e099d8
Lo <2639198987@qq.com>
2024-10-29 22:56:28 +0800

    1

--- ruoyi-ui/src/views/carbonFootprint/modelIndex/index.vue
+++ ruoyi-ui/src/views/carbonFootprint/modelIndex/index.vue
@@ -1,27 +1,11 @@
 <template>
-  <div id="app" style="width: 100%;display: flex">
-    <div class="sidebar">
-      <h3>模型管理</h3>
-      <div class="button-group">
-        <el-button type="primary" @click="currentView = 'createModel'">新建模型</el-button>
-        <el-button type="info" @click="currentView = 'editModel'">编辑模型</el-button>
-        <el-button type="success" @click="currentView = 'viewModel'">查看模型</el-button>
-        <el-button type="danger" @click="currentView = 'deleteModel'">删除模型</el-button>
-      </div>
+  <div style="margin: 25px">
+    <div class="button_class">
+      <el-button type="primary">新建</el-button>
+      <el-button type="primary">删除</el-button>
     </div>
-    <div class="content">
-      <div v-if="currentView === 'createModel'">
-        <el-image src="/images/smzqmxgl.png"></el-image>
-      </div>
-      <div v-if="currentView === 'editModel'">
-        <el-image src="/images/smzqmxgl.png"></el-image>
-      </div>
-      <div v-if="currentView === 'viewModel'">
-        <el-image src="/images/smzqmxgl.png"></el-image>
-      </div>
-      <div v-if="currentView === 'deleteModel'">
-        <el-image src="/images/smzqmxgl.png"></el-image>
-      </div>
+    <div>
+      <el-image src="/images/smzq.png"></el-image>
     </div>
   </div>
 </template>
@@ -29,44 +13,14 @@
 <script>
   export default {
     data() {
-      return {
-        currentView: 'createModel' // 默认显示的新建模型内容
-      };
+      return {};
     }
   };
 </script>
 
 <style scoped>
-  .main-container {
-
-  }
-
-  .sidebar {
-    width: 250px;
-    border-right: 2px solid #eaeaea;
-    padding: 20px;
-    background-color: #f5f5f5;
-    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
-  }
-
-  .sidebar h3 {
-    margin-bottom: 20px;
-    text-align: center; /* 居中标题 */
-  }
-
-  .button-group {
+  .button_class {
     display: flex;
-    flex-direction: column; /* 竖直排列 */
-  }
-
-  .el-button {
-    margin: 10px 0; /* 每个按钮间隔 */
-    width: 100%; /* 按钮等宽填满 */
-  }
-
-  .content {
-    flex: 1;
-    padding: 20px;
-    height: 100vh;
+    align-items: center;
   }
 </style>
